43e5653453b2f8e7d75c1e86d355a515ef095868
Shadaj Laddad <shadaj@users.noreply.github.com>
2015-06-27 12:58:05 -0700

    Part 2

--- src/main/scala/collidium/HelloCollidium.scala
+++ src/main/scala/collidium/HelloCollidium.scala
@@ -1,13 +1,19 @@
 package collidium
 
+import org.scalajs.dom.html
+import org.scalajs.dom.raw.CanvasRenderingContext2D
+
 import scala.scalajs.js
 import js.annotation.JSExport
 import org.scalajs.dom
 
 object HelloCollidium extends js.JSApp {
   def main(): Unit = {
-    val paragraph = dom.document.createElement("p")
-    paragraph.innerHTML = "<strong>It works!</strong>"
-    dom.document.getElementById("playground").appendChild(paragraph)
+    val canvasElem = dom.document.getElementById("canvas").asInstanceOf[html.Canvas]
+    val canvasContext = canvasElem.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+    canvasContext.beginPath()
+    canvasContext.moveTo(0, 0)
+    canvasContext.lineTo(500, 500)
+    canvasContext.stroke()
   }
 }
